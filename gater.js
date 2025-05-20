a3ad7a1dba5976597e48064923dd0b10f6e6fbec
Om Mehta <ommehta16@icloud.com>
2025-05-19 23:53:22 -0400

    Add website?

---
+++ gater.js
@@ -0,0 +1,301 @@
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+
+const snapDist = 40;
+let clicked = false;
+
+let mouseX = 0;
+let mouseY = 0;
+document.addEventListener("mousemove", e => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+})
+
+document.addEventListener("mousedown", () => { clicked = true; })
+document.addEventListener("mouseup", () => { clicked = false; })
+
+document.querySelector("#canvas-container").appendChild(canvas);
+
+class Drawable {
+    render() { }
+}
+
+class Renderer {
+    #toDraw = [];
+
+    constructor() {
+    }
+
+    /** @param {Drawable} drawable */
+    draw(drawable) {
+        this.#toDraw.push(drawable);
+        this.render();
+    }
+
+    /** @param {Drawable} drawable */
+    remove(drawable) {
+        this.#toDraw = this.#toDraw.filter(itm => itm != drawable);
+        this.render();
+    }
+
+    render() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.#toDraw.forEach(item => item.render());
+    }
+}
+
+const renderer = new Renderer();
+
+class Point extends Drawable {
+    x = 0;
+    y = 0;
+    radius = 0;
+    color = "#fff";
+
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} radius 
+     */
+    constructor(x, y, radius = 0, color = "fff") {
+        super();
+
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    render() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.ellipse(this.x, this.y, this.radius, this.radius, 0, 2 * Math.PI, 0);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+    copy() {
+        return new Point(this.x, this.y, this.radius, this.color);
+    }
+}
+
+class Line extends Drawable {
+    pt1;
+    pt2;
+    // ^ Point
+    color;
+
+    /**
+     * @param {Point} pt1 
+     * @param {Point} pt2 
+     */
+    constructor(pt1, pt2, color = "#fff") {
+        super();
+
+        this.pt1 = pt1;
+        this.pt2 = pt2;
+        this.color = color;
+    }
+
+    render() {
+        this.pt1.render();
+        this.pt2.render();
+
+        ctx.beginPath();
+        ctx.moveTo(this.pt1.x, this.pt1.y);
+        ctx.lineTo(this.pt2.x, this.pt2.y);
+        ctx.closePath();
+
+        ctx.strokeStyle = this.color;
+        ctx.stroke();
+    }
+
+    copy() {
+        let copied1 = this.pt1.copy();
+        let copied2 = this.pt2.copy();
+
+        let copied = new Line(copied1, copied2, this.color);
+        return copied;
+    }
+}
+
+let ratio = window.devicePixelRatio;
+
+function resizeCanvas() {
+    ratio = window.devicePixelRatio;
+    canvas.width = window.innerWidth * ratio;
+    canvas.height = window.innerHeight * ratio;
+    renderer.render();
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+let start = new Point(100, 100, 10);
+let end = start;
+
+const curr = new Line(start, end, "red");
+renderer.draw(curr);
+
+let inpConnected = new Set();
+let outConnected = new Set();
+
+let inpFree = new Set();
+let outFree = new Set();
+
+
+const distance = (x1, y1, x2, y2) => Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+
+const getSnap = (e) => {
+    let lowest = snapDist;
+    let closePt = null;
+    /** @param {Point} point */
+    const checkNearest = point => {
+        const dist = distance(point.x, point.y, e.clientX * ratio, e.clientY * ratio);
+        if (dist >= lowest) return;
+        lowest = dist;
+        closePt = point;
+    }
+
+
+    if (curr.pt1 == curr.pt2) { // we still need to place first node
+        outFree.forEach(checkNearest);
+        outConnected.forEach(checkNearest);
+        return closePt;
+    }
+    inpFree.forEach(checkNearest);
+    freeClose = closePt;
+    inpConnected.forEach(checkNearest);
+
+    return closePt;
+}
+
+canvas.addEventListener("click", e => {
+    let snapPoint = getSnap(e);
+
+    if (snapPoint == null) return;  //snapPoint = new Point(e.clientX * ratio, e.clientY * ratio, 10);
+
+    if (curr.pt1 == curr.pt2) {
+        curr.pt1 = snapPoint;
+        if (outFree.has(snapPoint)) {
+            outFree.delete(snapPoint);
+            outConnected.add(snapPoint);
+        }
+        return;
+    }
+
+    // we have to close out the line and finalize it
+
+    let finalized = curr.copy();
+    finalized.pt1 = curr.pt1;
+    finalized.pt2 = snapPoint;
+
+    curr.pt2 = curr.pt1 = snapPoint.copy();
+
+    if (inpFree.has(snapPoint)) {
+        inpFree.delete(snapPoint);
+        inpConnected.add(snapPoint);
+    }
+    else {
+        console.log("should delete the other line");
+        renderer.remove(snapPoint.lineTo);
+    }
+
+    finalized.pt1.lineTo = finalized;
+    finalized.pt2.lineTo = finalized;
+    finalized.color = "#fff";
+    renderer.draw(finalized);
+});
+
+canvas.addEventListener("mousemove", e => {
+    let snapPoint = getSnap(e);
+    if (snapPoint == null) snapPoint = new Point(e.clientX * ratio, e.clientY * ratio, 10);
+
+    curr.pt2.x = snapPoint.x;
+    curr.pt2.y = snapPoint.y;
+
+    renderer.render();
+})
+
+const clamp = (a, lo, hi) => Math.max(Math.min(a, hi), lo);
+
+class LogicNode extends Drawable {
+    inputs = [];
+    outputs = [];
+    #func;
+    element;
+
+    /**
+     * @param {number} inputs 
+     * @param {number} outputs 
+     * @param {Function} func 
+     */
+    constructor(inputs, outputs, name, func) {
+        super();
+
+        for (let i = 0; i < inputs; i++) this.inputs.push(new Point(0, 0, 10));
+        for (let i = 0; i < outputs; i++) this.outputs.push(new Point(0, 0, 10));
+        this.inputs.forEach(pt => inpFree.add(pt));
+        this.outputs.forEach(pt => outFree.add(pt));
+        this.#func = func;
+        this.name = name;
+
+        this.element = document.createElement("div");
+        document.querySelector("#draggables").appendChild(this.element);
+        this.element.innerText = name;
+        this.element.addEventListener("mousemove", e => {
+            if (!clicked) return;
+            this.element.style.setProperty("--xctr", clamp(e.clientX, 0, window.innerWidth) + "px");
+            this.element.style.setProperty("--yctr", clamp(e.clientY, 0, window.innerHeight) + "px");
+
+            for (let i = 1; i <= 5; i++) {
+                setTimeout(() => {
+                    this.element.style.setProperty("--xctr", clamp(mouseX, 0, window.innerWidth) + "px");
+                    this.element.style.setProperty("--yctr", clamp(mouseY, 0, window.innerHeight) + "px");
+                    renderer.render();
+                }, 25 * i)
+            }
+            renderer.render();
+        });
+        renderer.draw(this);
+    }
+
+    updatePoints() {
+        let elLeft = this.element.getBoundingClientRect().left;
+        let elRight = this.element.getBoundingClientRect().right;
+        let elTop = this.element.getBoundingClientRect().top;
+        let elBottom = this.element.getBoundingClientRect().bottom;
+
+        let lIncr = (elBottom - elTop) / (this.inputs.length + 1);
+        this.inputs.forEach((point, i) => {
+            point.x = (elLeft - 8) * ratio;
+            point.y = (elTop + (i + 1) * lIncr) * ratio;
+        });
+
+        let rIncr = (elBottom - elTop) / (this.outputs.length + 1);
+        this.outputs.forEach((point, i) => {
+            point.x = (elRight + 8) * ratio;
+            point.y = (elTop + (i + 1) * rIncr) * ratio;
+        });
+    }
+
+    render() {
+        this.updatePoints();
+        this.inputs.forEach(itm => itm.render());
+        this.outputs.forEach(itm => itm.render());
+    }
+}
+
+let nodes = [];
+
+/**
+ * @param {string} type 
+ */
+function addNode(type) {
+    if (type == "and") nodes.push(new LogicNode(2, 1, "AND", a => (a[0] && a[1])));
+    if (type == "or") nodes.push(new LogicNode(2, 1, "OR", a => (a[0] || a[1])));
+    if (type == "not") nodes.push(new LogicNode(1, 1, "NOT", a => !a[0]));
+}
+
+document.querySelector("#add-and").addEventListener("click", addNode.bind(this, "and"));
+document.querySelector("#add-or").addEventListener("click", addNode.bind(this, "or"));
+document.querySelector("#add-not").addEventListener("click", addNode.bind(this, "not"));
